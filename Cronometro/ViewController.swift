512aa342764c1a5dddb5c25f4aeb93d78264fa4a
Rafael Ornelas <rornelas@macrafael.local>
2015-09-22 16:30:10 -0300

    Commit app Cronometro GitHub

--- Cronometro/ViewController.swift
+++ Cronometro/ViewController.swift
@@ -9,15 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var time = 0
+    var timer = NSTimer()
+
+    @IBOutlet weak var timeLabel: UILabel!
+    
+    @IBAction func play(sender: AnyObject) {
+
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: Selector("increaseTime"), userInfo: nil, repeats: true)
+    }
+
+    @IBAction func pause(sender: AnyObject) {
+        timer.invalidate()
+    }
+    
+    @IBAction func zerar(sender: AnyObject) {
+        timer.invalidate()
+        time = 0
+        timeLabel.text = "\(0)"
+    }
+    
+    func increaseTime() {
+        time++
+        timeLabel.text = "\(time)"
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
 
